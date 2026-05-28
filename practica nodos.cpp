59c1b7222a62a4196efcc02e291b035d5f6be84e
Felipe Rodriguez <luismatarrita64@gmail.com>
2026-05-27 18:22:45 -0600

    segunda practica de nodos

---
+++ practica nodos.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct CuentaBancaria {
+    int numeroCuenta;
+    string nombreCliente;
+    double saldo;
+};
+
+//Nodo para almacenar datos
+struct Nodo {
+    //dato
+    CuentaBancaria cuenta;
+    //enlace
+    Nodo* siguiente;
+
+};
+
+class ListaEnlazadaSimple {
+
+private:
+    Nodo* inicio;
+
+public:
+    ListaEnlazadaSimple() {
+        inicio = nullptr;
+    }
+
+    void agregar(Nodo* nuevoNodo) {
+        if (inicio == nullptr) {
+            inicio = nuevoNodo;
+        }
+        else {
+            //Tomamos la referencia del primer elemento de la lista 
+            Nodo* nodoAuxiliar = inicio;
+
+            //Avanzamos hasta encontrar el ultimo de la lista
+            while (nodoAuxiliar->siguiente != nullptr)
+            {
+                //Avanzamos elsiguiente Nodo
+                nodoAuxiliar = nodoAuxiliar->siguiente;
+            }
+        }
+
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
